8ad7ce610f2824b50d96b041d316b1cf32b343d6
prolley-parnell <prolleyp@gmail.com>
2025-02-24 18:23:27 +0000

    Add code to analyse files

---
+++ scripts/analysis.py
@@ -0,0 +1,38 @@
+###
+## Functions to analyse the collision class
+###
+
+import argparse
+from pathlib import Path
+from src.animal import AnimalStruct
+from src.object import CollisionObj
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Plot antennal contacts over time", exit_on_error=False)
+    parser.add_argument("folder", help="Path to the datetime folder")
+    parser.add_argument("prefix", type=str, help="Datetime prefix (example: '240905-1616')")
+    parser.add_argument("session", type=int, help="Session number (example: 28)")
+    parser.add_argument("skeleton", type=str, help="skeleton.toml (An example in the top level of this repo)")
+
+
+    try:
+        args = parser.parse_args()
+    except:
+        # We're running from the console
+        from dataclasses import dataclass
+        @dataclass
+        class args:
+            folder = Path('/Users/persie/PhD_Code/3d_ant_data_rle')
+            prefix = '240905-1616'
+            session = 28
+            skeleton = '../skeleton.toml'
+
+    PREFIX = args.prefix
+    FOLDER = Path(args.folder) / PREFIX
+    SESSION = args.session
+
+
+## Generate the Animal and Object Instances
+
