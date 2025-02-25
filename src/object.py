df5570c610ed1a79b1743edf67e43aa823b52c2c
prolley-parnell <prolley-parnell@gmail.com>
2025-02-25 16:38:13 +0000

    Add code to try shape matching

--- src/object.py
+++ src/object.py
@@ -7,26 +7,28 @@ logger = logging.getLogger(__name__)
 
 class CollisionObj:
 
-    def __init__(self, obj_folder: str, units: str = "mm"):
-        '''
+    def __init__(self, obj_folder: str, units: str = "m"):
+        """
         Initialise an object given by a folder of "obj" files into a dictionary of trimesh objects.
         :param obj_folder: Path to the folder containing the objects
-        '''
+        :param units: Units of the object as the default if none is specified in the file
+        """
         self._obj_folder = obj_folder
         self._obj_dict = {}
         self._units = units
         self._read_obj_folder_mt(self._obj_folder)
+        self.frames = sorted(self._obj_dict.keys())
 
     def check_frame_exist(self, frame_idx):
-        ''' If frame index is not present in the object dictionary, return false'''
+        """ If frame index is not present in the object dictionary, return false"""
         if not self._obj_dict.keys().__contains__(frame_idx):
             logger.debug('Frame index {} not present in the object mesh dictionary \n'.format(frame_idx))
             return False
         return True
 
     def _read_obj_folder(self, obj_dir_path: str):
-        ''' Given a path to a folder containing ".obj" files with the name of the corresponding frame, load these and
-        convert to a dict of trimesh'''
+        """ Given a path to a folder containing ".obj" files with the name of the corresponding frame, load these and
+        convert to a dict of trimesh"""
         # scan all the OBJ file names in this directory
         path_list = os.listdir(obj_dir_path)
 
@@ -36,8 +38,8 @@ class CollisionObj:
 
 
     def _read_obj_folder_mt(self, obj_dir_path: str):
-        ''' Given a path to a folder containing ".obj" or ".dae" files with the name of the corresponding frame, load these and
-        convert to a dict of trimesh'''
+        """ Given a path to a folder containing ".obj" or ".dae" files with the name of the corresponding frame, load these and
+        convert to a dict of trimesh"""
         # scan all the OBJ file names in this directory
         path_list = os.listdir(obj_dir_path)
 
@@ -56,15 +58,15 @@ class CollisionObj:
 
 
     def _single_obj_to_trimesh(self, obj_path: str):
-        ''' Convert the dae at the given path to a trimesh object and return the object and frame index '''
+        """ Convert the dae or obj at the given path to a trimesh object and return the object and frame index """
         if os.path.isdir(self._obj_folder + obj_path):
             raise Exception("Folder {} ignored".format(self._obj_folder + obj_path))
         if os.path.splitext(obj_path)[-1] in [".dae", ".DAE"] and not obj_path.startswith('.'):
             frame_index = int(os.path.splitext(obj_path)[-2].split('_')[-1]) # Add to only get the number next to the obj extension
-            trimesh_obj = trimesh.load_mesh(self._obj_folder + obj_path, 'dae')
+            trimesh_obj = trimesh.load_mesh(self._obj_folder + obj_path, 'dae', process=False)
         elif os.path.splitext(obj_path)[-1] in [".obj", ".OBJ"] and not obj_path.startswith('.'):
             frame_index = int(os.path.splitext(obj_path)[-2].split('_')[-1]) # Add to only get the number next to the obj extension
-            trimesh_obj = trimesh.load_mesh(self._obj_folder + obj_path, 'obj')
+            trimesh_obj = trimesh.load_mesh(self._obj_folder + obj_path, 'obj', process=False)
         else:
             raise Exception("File {} ignored, not .dae or .obj file".format(self._obj_folder + obj_path))
 
@@ -76,87 +78,84 @@ class CollisionObj:
         return frame_index, trimesh_obj
 
     def get_frame_range(self):
-        ''' Give [min, max] of the frame indices for the stored poses'''
+        """ Give [min, max] of the frame indices for the stored poses"""
         return [min(self._obj_dict.keys()), max(self._obj_dict.keys())]
 
-    def generate_geometry(self, frame_idx: int):
-        '''Return a trimesh of the object at the given frame index'''
+    def generate_geometry(self, frame_idx: int) -> trimesh.Trimesh | None:
+        """Return a trimesh of the object at the given frame index"""
         if self.check_frame_exist(frame_idx):
             return self._obj_dict[frame_idx]
         else:
             return None
 
     def generate_scene(self, frame_idx: int):
-        '''Return a trimesh scene object with the seed in the world frame'''
+        """Return a trimesh scene object with the seed in the world frame"""
         scene = trimesh.Scene()
         scene.add_geometry(self.generate_geometry(frame_idx), node_name="object", geom_name=str(frame_idx))
         return scene
 
 
 class CollisionObjTransform:
-    def __init__(self, obj_folder: str, units: str = "mm"):
-        '''
+    def __init__(self, obj_folder: str, units: str = "m"):
+        """
         Initialise an object given by a folder of "obj" files into a dictionary of trimesh objects.
         Convert from dict of trimesh into a single mesh and a dict of transforms
         :param obj_folder: Path to the folder containing the objects
-        '''
+        """
         self._obj_folder = obj_folder
         self._units = units
         co = CollisionObj(obj_folder, units)
-        self._obj_mesh, self._obj_transforms = convertCO(co)
+        self.obj_mesh, self._obj_transforms = convert_collision_obj(co)
 
 
 
     def check_frame_exist(self, frame_idx):
-        ''' If frame index is not present in the object dictionary, return false'''
-        if not self._obj_dict.keys().__contains__(frame_idx):
+        """ If frame index is not present in the object dictionary, return false"""
+        if not self._obj_transforms.keys().__contains__(frame_idx):
             logger.debug('Frame index {} not present in the object mesh dictionary \n'.format(frame_idx))
             return False
         return True
 
     def get_frame_range(self):
-        ''' Give [min, max] of the frame indices for the stored poses'''
-        return [min(self._obj_dict.keys()), max(self._obj_dict.keys())]
+        """ Give [min, max] of the frame indices for the stored poses"""
+        return [min(self._obj_transforms.keys()), max(self._obj_transforms.keys())]
 
-    def generate_geometry(self, frame_idx: int):
-        '''Return a trimesh of the object at the given frame index'''
+    def generate_transform(self, frame_idx: int):
+        """Return the transform of the object at the given frame index"""
         if self.check_frame_exist(frame_idx):
-            return self._obj_dict[frame_idx]
+            return self._obj_transforms[frame_idx]
         else:
             return None
 
     def generate_scene(self, frame_idx: int):
-        '''Return a trimesh scene object with the seed in the world frame'''
+        """Return a trimesh scene object with the seed in the world frame"""
         scene = trimesh.Scene()
-        scene.add_geometry(self.generate_geometry(frame_idx), node_name="object", geom_name=str(frame_idx))
+        scene.add_geometry(self.obj_mesh, node_name="object", geom_name=str(frame_idx), transform=self.generate_transform(frame_idx))
         return scene
 
 
-def convertCO(co: CollisionObj):
-    ''' Convert from a CollisionObject with a dict of objects, into a single trimesh object and a dict of transforms'''
+def convert_collision_obj(co: CollisionObj):
+    """ Convert from a CollisionObject with a dict of objects, into a single trimesh object and a dict of transforms"""
 
-    output_mesh = trimesh.Trimesh()
     output_transforms = dict()
 
-    #First find transforms
-    sorted_frames = sorted(co._obj_dict.keys())
+    sorted_frames = co.frames
+    origin = trimesh.creation.axis(origin_size=1, axis_length=4)
+    geom_prior = co.generate_geometry(sorted_frames[0])
+    output_transforms[sorted_frames[0]] = geom_prior.principal_inertia_transform
 
-    geom_prior = None
-    for frame in sorted_frames:
+    origin.apply_transform(output_transforms[sorted_frames[0]])
+    
+    for frame in sorted_frames[1:14]:
+
         geom_base = co.generate_geometry(frame)
+        print(geom_prior.principal_inertia_vectors)
+        T, cost = geom_prior.register(geom_base)
+        geom_prior.apply_transform(T)
+        origin.apply_transform(T)
+        print(cost)
+        output_transforms[frame] = origin.principal_inertia_transform
 
-        if geom_prior is None:
-            if geom_base is None:
-                raise Exception("No prior geometry for frame {}".format(frame))
-            else:
-                output_transforms[frame] = geom_base.principal_inertia_transform
-                geom_prior = geom_base
-        else:
-            T, cost = geom_prior.register(geom_base)
-            print(cost)
-            output_transforms[frame] = T
-
-
-
+    output_mesh = geom_prior
 
     return output_mesh, output_transforms
