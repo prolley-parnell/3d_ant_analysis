07306328cd976440af1ccc83925d9456eb493d94
prolley-parnell <prolley-parnell@gmail.com>
2025-02-26 12:31:16 +0000

    Add window close event handler

--- src/visual_widget.py
+++ src/visual_widget.py
@@ -112,6 +112,12 @@ class MultiViewer:
             config = pyglet.gl.Config(double_buffer=True)
             window = pyglet.window.Window(config=config, width=width, height=height)
 
+        event_loop = pyglet.app.EventLoop()
+
+        @event_loop.event
+        def on_window_close(window):
+            event_loop.exit()
+
         @window.event
         def on_key_press(symbol, modifiers):
             if modifiers == 0:
